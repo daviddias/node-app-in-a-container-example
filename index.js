8b68a5506f161a5762d6088359b6ca2cf249cb62
David Dias <daviddias.p@gmail.com>
2014-01-30 19:50:02 +0000

    initial import

---
+++ index.js
@@ -0,0 +1,10 @@
+var http = require('http');
+
+var port = process.env.PORT || 1337;
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(port);
+
+console.log('Server running at :'+ port);
